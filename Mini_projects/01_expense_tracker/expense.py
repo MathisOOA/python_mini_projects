40c74d6da605bd11d2f7a7d2d151608d8b19d4dc
MathisOOA <Mathis-ada@hotmail.com>
2026-07-05 16:00:25 -0400

    Add Expense class and implement view_expense method

---
+++ Mini_projects/01_expense_tracker/expense.py
@@ -0,0 +1,15 @@
+class Expense :
+
+    current_id = 1 #Variable local permettant de calculer le nbr d'instance d'expense crée
+
+    def __init__(self,date,name,amount): #Constructeur
+        self.id = Expense.current_id
+        Expense.current_id += 1
+
+        self.date = date
+        self.name = name
+        self.amount = amount 
+
+
+    def __str__(self): #Équivalent toString() en java
+        return f"{self.id:<5} {self.name:<15} {self.date:<25} {self.amount:>5.2f}$"
--- Mini_projects/01_expense_tracker/expense_tracker.py
+++ Mini_projects/01_expense_tracker/expense_tracker.py
@@ -1,28 +1,28 @@
 from datetime import datetime
+from expense import Expense
 
 class ExpenseTracker:
-    expense_list = []
+    expense1 = Expense('07/05/2026, 15:43:26', 'chocolat', 21.3)
+    expense2 = Expense('11/21/2026, 05:03:26', 'Cacao', 8.32)
+    expense_list = [expense1,expense2]
 
     def display_menu(self) : 
 
         input_user = 0 
 
         while(input_user != 5) :
-            print("""====== Expense Tracker ======
-            1. Add Expense
-            2. Delete Expense
-            3. View Expenses
-            4. Show Total Spent
-            5. Exit       
+            print("""\n====== Expense Tracker ======
+        1. Add Expense
+        2. Delete Expense
+        3. View Expenses
+        4. Show Total Spent
+        5. Exit       
             """)
 
             input_user = int(input('Choose an option: '))
 
             if(input_user == 1) :
                 self.add_expense()
-
-                #Test (À ENLEVER)
-                print('ici : ',len(self.expense_list))
 
             elif(input_user == 2) :
                 self.delete_expense()
@@ -47,26 +47,27 @@ class ExpenseTracker:
 
         print('Please enter the following details :')
         expense_name = input('Expense name : ')
-        amount = float(input('Amount : ')) #gérer les cas où l'utilisateur n'entre pas une valeur convenable
+        amount = float(input('Amount : ')) #Gérer les cas où l'utilisateur n'entre pas une valeur convenable
 
-        object_information = {
-            'expense_date' : date,
-            'expense_name' : expense_name,
-            'expense_amount' : amount
-        }
-
-        self.expense_list.append(object_information)
+        expense = Expense(date,expense_name,amount)
+        self.expense_list.append(expense)
     
     def view_expense(self) :
-        pass
+        print("""
+ID      Name         Date("m/d/Y, H:M:S")     Amount
+------------------------------------------------------""")
+        
+        for expense in self.expense_list :
+            print(expense)
+        
 
     def show_total_spent(self) :
         total_expense = 0
 
         for expense in self.expense_list :
-            total_expense += expense['expense_amount']
+            total_expense += expense.amount
         
-        print('ici',total_expense)
+        print('You spent in total {:.2f} $'.format(total_expense))
 
 
     def delete_expense(self) :
